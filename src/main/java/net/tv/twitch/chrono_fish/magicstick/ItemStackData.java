4ba6d5b446a47364b980f9ab4adc0bc384433bc4
Daka-fish <yamatohidaka24@gmail.com>
2024-04-08 23:10:01 +0900

    かえん斬りの耐久値調整

--- src/main/java/net/tv/twitch/chrono_fish/magicstick/ItemStackData.java
+++ src/main/java/net/tv/twitch/chrono_fish/magicstick/ItemStackData.java
@@ -3,13 +3,17 @@ package net.tv.twitch.chrono_fish.magicstick;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemStackData {
-    static ItemStack flameSlash = new ItemStack(Material.GOLDEN_SWORD);
+    static Material goldSword = Material.GOLDEN_SWORD;
 
     public static ItemStack getFlameSlash(){
+        ItemStack flameSlash = new ItemStack(goldSword);
         ItemMeta flameMeta = flameSlash.getItemMeta();
+        Damageable flameDamage = (Damageable) flameMeta;
+        flameDamage.setDamage(goldSword.getMaxDurability()-1);
         flameMeta.addEnchant(Enchantment.FIRE_ASPECT,1,true);
         flameMeta.setDisplayName("§cかえん斬り");
         flameSlash.setItemMeta(flameMeta);
